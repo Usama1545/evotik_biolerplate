d750fa5a9438ca1544dfb7c3400f6199759c4d82
DanishSherazi <danishsherazi5121472@gmail.com>
2024-08-08 00:33:47 +0500

    updated AppServiceProvider

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -35,57 +35,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->app->singleton('memo', function ($app) {
-            return new MemoService();
-        });
-
-        Order::observe(OrderObserver::class);
-
         Request::macro('language', function () {
             $lang = $this->header('language') ?? 'en';
 
             return Language::all()->pluck('iso_2')->contains($lang) ? $lang : 'en';
-        });
-
-        Carbon::setLocale('ar');
-
-        Gate::define('update-order-status', function (User $user, Order $order, ?OrderStatusEnum $status = null) {
-            $pass = false;
-            switch ($status) {
-                case OrderStatusEnum::DELIVERED:
-                    $pass = $order->is_seller;
-                    break;
-                case OrderStatusEnum::APPROVED:
-                    $pass = $order->is_buyer;
-                    break;
-                case OrderStatusEnum::REQUESTED_REVISION:
-                    $pass = $order->is_buyer;
-                    break;
-                case OrderStatusEnum::CANCELLED:
-                    $pass = $order->is_buyer || $order->is_seller;
-                    break;
-                default:
-                    $pass = $user->is_admin;
-                    break;
-            }
-            return $pass;
-        });
-
-
-        VerifyEmail::createUrlUsing(function ($notifiable) {
-            $path = URL::temporarySignedRoute(
-                'verification.verify',
-                Carbon::now()->addMinutes(Config::get('auth.verification.expire', 60)),
-                [
-                    'id' => $notifiable->getKey(),
-                    'hash' => sha1($notifiable->getEmailForVerification()),
-                ],
-                absolute: false
-            );
-
-            $url = config('app.front_app_url') . str_replace('api/', '', $path);
-
-            return $url;
-        });
+        });    
     }
 }
